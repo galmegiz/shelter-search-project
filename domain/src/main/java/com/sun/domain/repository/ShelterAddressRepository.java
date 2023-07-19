d134c65b0c275167ef9b4edab3ad3fc71efc671c
galmegiz <somethingsjw@gmail.com>
2023-07-20 00:28:24 +0900

    feat: feature#1 - 2 csv 데이터 db 저장 구현 - ShelterAddressDto -> ShelterAddress(Entity)로 변환 후 데이터베이스 저장 로직 구현 - 현재 정적 리소스 경로에서 데이터를 저장하지만 향후 데이터 경로를 받아 저장할 수 있게끔 수정하는 게 나을 듯함

---
+++ domain/src/main/java/com/sun/domain/repository/ShelterAddressRepository.java
@@ -0,0 +1,7 @@
+package com.sun.domain.repository;
+
+import com.sun.domain.entity.ShelterAddress;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShelterAddressRepository extends JpaRepository<ShelterAddress, Long> {
+}
