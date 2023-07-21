55dfbf5b7c9aff73f81dbd040757be965e61cdb0
galmegiz <somethingsjw@gmail.com>
2023-07-21 23:29:04 +0900

    refactor: presentation 모듈이 jpa관련 모듈의 존하지 않도록 환경설정 파일 분리 repository, entity scan annotation 위치를 domain 모듈로 이동

--- presentation/src/main/java/com/sun/presentation/Application.java
+++ presentation/src/main/java/com/sun/presentation/Application.java
@@ -3,14 +3,11 @@ package com.sun.presentation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication(scanBasePackages = {"com.sun"})
 @ConfigurationPropertiesScan(basePackages = {"com.sun"})
-@EntityScan(basePackages = "com.sun")
-@EnableJpaRepositories(basePackages = {"com.sun"})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
