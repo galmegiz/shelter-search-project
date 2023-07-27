b9c6fd3b2b7fd160544cda84ad1f8081c62209aa
galmegiz <somethingsjw@gmail.com>
2023-07-27 21:27:11 +0900

    feat: feature#8-2 요청에 대한 입력값 검증 및 에러코드, 예외처리 로직 추가

--- presentation/src/main/java/com/sun/presentation/exception/GlobalExceptionHandler.java
+++ presentation/src/main/java/com/sun/presentation/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import com.sun.presentation.constant.ResponseCode;
 import com.sun.presentation.dto.Response;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -16,6 +17,13 @@ public class GlobalExceptionHandler {
     public ResponseEntity<Response> CoordinateNotFoundExHandler(CoordinateNotFoundException ex) {
         log.error("API 검색 결과 {}에 대한 좌표결과가 없음", ex.getSearchQuery(), ex);
         Response response = Response.fail(ResponseCode.COORDINATE_NOT_FOUND, "query=" + ex.getSearchQuery());
+        return ResponseEntity.status(response.getResponseCode().getHttpStatus()).body(response);
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<Response> CoordinateNotFoundExHandler(MissingServletRequestParameterException ex) {
+        log.error("API 검색 결과 {}에 대한 좌표결과가 없음", ex);
+        Response response = Response.fail(ResponseCode.REQUEST_VALIDATION_ERROR);
         return ResponseEntity.status(response.getResponseCode().getHttpStatus()).body(response);
     }
 }
